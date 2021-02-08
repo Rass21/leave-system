2db0ef2101d6bf6423fcbbc5b4f04d4913babf1b
Rasmus Patomella <Rasmus.Patomella@itbyn.se>
2021-02-08 12:31:39 +0100

    Added sidebar icons and javascript for animations. Edited login and registration page. Smoothed out datatables. Added icons for leave requests

--- leave-system/Controllers/HomeController.cs
+++ leave-system/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using leave_system.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace leave_system.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ leave-system/Views/Shared/_AdminLTE.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace leave_system.Views.Shared
+{
+    public class _AdminLTEModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
